16970b773919891d103f82b16bfef69764eb4577
Amir Tanvir <amirtanvir96@gmail.com>
2023-02-18 14:59:40 +0000

    housesnapv2

--- src/app/components/services.jsx
+++ src/app/components/services.jsx
@@ -36,7 +36,7 @@ const Services = () => {
                             <h3 className="mb-2 text-xl font-bold dark:text-white">Virtual Staging</h3>
                             <p className="text-gray-500 dark:text-gray-400">We understand the importance of presenting a well-designed and inviting space when selling a property. 
                             That&apos;s why we offer virtual staging services to help you bring your empty or minimally furnished space to life. With our virtual staging, you can 
-                            showcase your property's full potential by adding virtual furniture and decor that accurately reflects your desired style and aesthetic. Our team 
+                            showcase your property&apos;s full potential by adding virtual furniture and decor that accurately reflects your desired style and aesthetic. Our team 
                             of experts uses advanced technology and design techniques to create a virtual staging experience that feels realistic and true to life, allowing potential
                             buyers to easily envision themselves in the space. Whether you&apos;re selling a new construction property or an older home, our virtual staging services
                             will help you sell your property faster and for more money.</p>
@@ -46,7 +46,7 @@ const Services = () => {
                             <h3 className="mb-2 text-xl font-bold dark:text-white">360° Virtual Tour</h3>
                             <p className="text-gray-500 dark:text-gray-400">Our virtual tours provide potential buyers with an immersive and interactive experience that 
                             allows them to explore your property from every angle. With our advanced 360° technology, viewers can navigate through each room and space, zoom
-                             in on details, and even view the property in a bird's eye perspective. Our team of experts uses high-quality equipment and techniques to create 
+                             in on details, and even view the property in a bird&apos;s eye perspective. Our team of experts uses high-quality equipment and techniques to create 
                              a seamless and visually stunning virtual tour that accurately reflects the look and feel of your property. 360° virtual tours are an effective way
                               to showcase the full potential of your property and are proven to increase engagement and drive more interest from potential buyers. Choose HouseSnap 
                               to provide you with a premium 360° virtual tour experience that will help you sell your property faster and for the best possible price.</p>
@@ -55,8 +55,8 @@ const Services = () => {
                         <div className='bg-gray-300 dark:bg-gray-700 dark:border-gray-700 dark:hover:bg-gray-700 p-8 rounded-md'>
                             <h3 className="mb-2 text-xl font-bold dark:text-white">Floor Plans</h3>
                             <p className="text-gray-500 dark:text-gray-400">At HouseSnap, we understand that accurate and detailed floor plans are a crucial component in showcasing 
-                            the layout and features of your property. That's why we offer professional floor plan services as part of our real estate photography offerings. Our team of 
-                            experienced photographers and designers use advanced technology and techniques to create detailed and precise floor plans that accurately represent your property's 
+                            the layout and features of your property. That&apos;s why we offer professional floor plan services as part of our real estate photography offerings. Our team of 
+                            experienced photographers and designers use advanced technology and techniques to create detailed and precise floor plans that accurately represent your property&apos;s 
                             layout. Our floor plans are customized to your specific property and can include features such as room dimensions, window and door placements, and other important details. 
                             Clear and accurate floor plans can help potential buyers visualize the property and its full potential, allowing them to make informed decisions about whether the property fits their specific needs. 
                             Choose HouseSnap to provide you with expert floor plans that will help you sell your property faster and for the best possible price.</p>
@@ -68,7 +68,7 @@ const Services = () => {
                     </video>
                     <div className='flex flex-col mt-10'>
                         <h1 className=' dark:text-white font-bold text-5xl px-10'>Breathtaking Drone Footage for one of our clients.</h1>
-                        <h2 className=' dark:text-white font-bolt text-4xl px-10 mt-10'>Shot in 4k</h2>
+                        <h2 className=' dark:text-white font-bolt text-4xl px- mt-10'>Shot in 4k</h2>
                     </div>
                 </div>
             </div>
